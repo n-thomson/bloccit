2785455ca55b78f197ae93544ad1d362cbb49e5b
Neethu Thomson <n***t@gmail.com>
2018-12-26 18:17:26 -0500

    interim submission with error

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -4,6 +4,7 @@ module.exports = {
   index(req, res, next){
     topicQueries.getAllTopics((err, topics) => {
       if(err){
+        console.log(err);
         res.redirect(500, "static/index");
       } else {
         res.render("topics/index", {topics});
