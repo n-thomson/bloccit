b81ae40f777aa520d65d093b4f615dfaa6e1f54b
Neethu Thomson <n***t@gmail.com>
2019-01-05 20:37:25 -0500

    assgn complete

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,4 +1,19 @@
 module.exports = {
+  validateTopics(req, res, next) {
+    if(req.method === "POST") {
+      req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
+      req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
+    }
+
+    const errors = req.validationErrors();
+    if (errors) {
+      req.flash("error", errors);
+      return res.redirect(303, req.headers.referer)
+    } else {
+      return next();
+    }
+  },
+
   validatePosts(req, res, next) {
     if(req.method === "POST") {
       req.checkParams("topicId", "must be valid").notEmpty().isInt();
